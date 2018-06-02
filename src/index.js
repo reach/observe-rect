9d33fe27026848c3ebecc67fbcb0ead25b586970
Benoît Zugmeyer <bzugmeyer@gmail.com>
2018-06-02 21:17:58 +0200

    improved performances
    This change tries to improve the performances in two ways:
    
    * Read the rect *after* the rAF is executed, because the browser usually
    makes a reflow after the rAF, so the getBoundingClientRect call won't
    trigger an extra reflow.  This is the solution explained
    [here](https://developer.mozilla.org/en-US/Firefox/Performance_best_practices_for_Firefox_fe_engineers#How_do_I_avoid_triggering_uninterruptible_reflow)
    
    * Run everything in a single rAF, because poping a lot of rAF and setTimeouts is CPU intensive.

--- src/index.js
+++ src/index.js
@@ -2,22 +2,59 @@ let props = ["width", "height", "top", "right", "bottom", "left"];
 
 let rectChanged = (a = {}, b = {}) => props.some(prop => a[prop] !== b[prop]);
 
-export default (node, cb) => {
-  let rect;
-  let rafId;
+let observedNodes = new Map();
+let rafId;
 
-  let observe = () => {
-    let newRect = node.getBoundingClientRect();
-    if (rectChanged(newRect, rect)) {
-      rect = newRect;
-      cb(rect);
+let run = () => {
+  observedNodes.forEach(state => {
+    if (state.hasRectChanged) {
+      state.callbacks.forEach(cb => cb(state.rect));
+      state.hasRectChanged = false;
     }
-    rafId = requestAnimationFrame(observe);
-  };
+  });
 
-  let unobserve = () => {
-    cancelAnimationFrame(rafId);
-  };
+  setTimeout(() => {
+    observedNodes.forEach((state, node) => {
+      let newRect = node.getBoundingClientRect();
+      if (rectChanged(newRect, state.rect)) {
+        state.hasRectChanged = true;
+        state.rect = newRect;
+      }
+    });
+  }, 0);
 
-  return { observe, unobserve };
+  rafId = requestAnimationFrame(run);
+};
+
+export default (node, cb) => {
+  return {
+    observe() {
+      let wasEmpty = observedNodes.size === 0;
+      if (observedNodes.has(node)) {
+        observedNodes.get(node).callbacks.push(cb);
+      } else {
+        observedNodes.set(node, {
+          rect: undefined,
+          hasRectChanged: false,
+          callbacks: [cb]
+        });
+      }
+      if (wasEmpty) run();
+    },
+
+    unobserve() {
+      let state = observedNodes.get(node);
+      if (state) {
+        // Remove the callback
+        const index = state.callbacks.indexOf(cb);
+        if (index >= 0) state.callbacks.splice(index, 1);
+
+        // Remove the node reference
+        if (!state.callbacks.length) observedNodes.delete(node);
+
+        // Stop the loop
+        if (!observedNodes.size) cancelAnimationFrame(rafId);
+      }
+    }
+  };
 };
