2459deb29a199282e4c6997cfb01173f809d50c7
Gabe Klein <gabe@termtm.com>
2020-09-25 18:33:48 -0600

    indent w spaces instead

--- src/index.ts
+++ src/index.ts
@@ -1,67 +1,67 @@
 type RectProps = {
-	rect: DOMRect;
-	callbacks: Set<Function>;
+  rect: DOMRect;
+  callbacks: Set<Function>;
 };
 
 let COMPARE_KEYS = [
-	"bottom", "height", "left", "right", "top", "width"
+  "bottom", "height", "left", "right", "top", "width"
 ] as const;
 
 let observedNodes = new Map<Element, RectProps>();
 let active: boolean;
 
 function checkForUpdates(){
-	if(active){
-		observedNodes.forEach(assertDidUpdate);
-		window.requestAnimationFrame(checkForUpdates);
-	}
+  if(active){
+    observedNodes.forEach(assertDidUpdate);
+    window.requestAnimationFrame(checkForUpdates);
+  }
 };
 
 function assertDidUpdate(state: RectProps, node: Element){
-	let newRect = node.getBoundingClientRect();
+  let newRect = node.getBoundingClientRect();
 
-	for(const key of COMPARE_KEYS)
-		if(newRect[key] !== state.rect[key]){
-			state.rect = newRect;
-			state.callbacks.forEach(cb => cb(state.rect))
-			break;
-		}
+  for(const key of COMPARE_KEYS)
+    if(newRect[key] !== state.rect[key]){
+      state.rect = newRect;
+      state.callbacks.forEach(cb => cb(state.rect))
+      break;
+    }
 }
 
 export default function observeRect(
-	node: Element,
-	callback: (rect: DOMRect) => void
+  node: Element,
+  callback: (rect: DOMRect) => void
 ){
-	let state = observedNodes.get(node);
+  let state = observedNodes.get(node);
 
-	return {
-		observe(){
-			if(state)
-				state.callbacks.add(callback);
-			else {
-				observedNodes.set(node, state = {
-					rect: {} as any,
-					callbacks: new Set([callback]),
-				});
-		
-				if(!active){
-					active = true;
-					checkForUpdates();
-				}
-			}
-		},
-		unobserve(){
-			if(state){
-				state.callbacks.delete(callback);
-	
-				if(!state.callbacks.size)
-					observedNodes.delete(node);
-	
-				state = undefined;
-	
-				if(!observedNodes.size)
-					active = false;
-			}
-		}
-	}
+  return {
+    observe(){
+      if(state)
+        state.callbacks.add(callback);
+      else {
+        observedNodes.set(node, state = {
+          rect: {} as any,
+          callbacks: new Set([callback]),
+        });
+
+        if(!active){
+          active = true;
+          checkForUpdates();
+        }
+      }
+    },
+    unobserve(){
+      if(state){
+        state.callbacks.delete(callback);
+
+        if(!state.callbacks.size)
+          observedNodes.delete(node);
+
+        state = undefined;
+
+        if(!observedNodes.size)
+          active = false;
+      }
+    }
+  }
 }
